85ae89638e5d86441abad659103360f7112db7ae
davidtaboh <davidtaboh@yahoo.com>
2020-04-25 18:25:29 -0400

    Saturday night add

--- static/Graphs.js
+++ static/Graphs.js
@@ -1,7 +1,41 @@
 // Read in data
 d3.csv("/Output/cleaneddata.csv").then(function(data) {
+
+  var x = document.getElementById("selDataset");
+  var y = document.getElementById("selDataset2");
+  for (let i = 0; i < data.length; i++) {
+    // Eliminate duplicates
+    if (data[i].region != data[i+1].region) {
+      var option = document.createElement("option");
+      option.text = data[i].region;
+      x.add(option);
+
+    }
+   
+
+  }
+
+  for (let i = 0; i < data.length; i++) {
+    // Eliminate duplicates
+    if (data[i].transportation_type != data[i+1].transportation_type) {
+      var option = document.createElement("option");
+      option.text = data[i].transportation_type;
+      y.add(option);
+
+    }
+
+  }
+
+  var inputField = d3.select("#selDataset2");
+  inputField.on("change", function () {
+    var user_pick_value = d3.event.target.value;
+    var select_id = document.getElementById("selDataset");
+    let city__pick = select_id.options[select_id.selectedIndex].value;
+
+    // Make sure to change variables below
+
     console.log(data);
-    let Belgium = data.filter(city => city.region == "Belgium" && city.transportation_type == "driving")
+    let Belgium = data.filter(city => city.region == city__pick && city.transportation_type == user_pick_value)
    console.log(Belgium)
     let belgium_date = Belgium.map(city_date => city_date.date);
     console.log(belgium_date)
@@ -27,6 +61,9 @@ d3.csv("/Output/cleaneddata.csv").then(function(data) {
 
        // // Render the plot to the div tag with id "plot"
      Plotly.newPlot("plot", [trace1], layout);
+  }
+
+  
 
    // let dateDriving = Belgium.map(driving => driving.transportation.)
    // console.log(dateDriving)
@@ -34,9 +71,10 @@ d3.csv("/Output/cleaneddata.csv").then(function(data) {
 });
 
 
-
 // var Belgium = data.region == "Belgium"
 // console.log(Belgium)
  
 //  // // Render the plot to the div tag with id "plot"
 //   Plotly.newPlot("plot", [trace1], layout);
+
+
